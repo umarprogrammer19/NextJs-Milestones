bf2ef38dada509fb9be94a7814b6c50663bf8b48
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-08-30 00:44:31 +0500

    Milestone 1 Completed

---
+++ Milestone-1/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './globals.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className='container'>
+      <h1 className='milestone'>Milestone 1 Achieved</h1>
+      <h1 className='helloWorld'>Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
